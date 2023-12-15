7b8ed100c2cba0b1ea7f2e5602ecbf4c04732df7
najiaokacha <100485014+najiaokacha@users.noreply.github.com>
2023-12-15 23:34:33 +0100

    Adding keratoconus model

--- database.py
+++ database.py
@@ -1,20 +1,15 @@
-import sys
-
-import cv2
-from PIL import Image
 import io
 import os
-from keras.models import load_model
-from PIL import Image
+import ssl
+import sys
+import cv2
 import numpy as np
 import pika
-import ssl
-import traceback
-
+from PIL import Image
+from keras.models import load_model
 from keras.optimizers import Adam
 
 #from receive import main
-from path import MQ
 
 # load model
 custom_objects = {'Adam': Adam}  # Replace 'Adam' with the actual optimizer used during training
@@ -22,8 +17,8 @@ custom_objects = {'Adam': Adam}  # Replace 'Adam' with the actual optimizer used
 modelPnemonia = load_model('pnemonia.h5', compile=False)
 model = load_model('brain-tumor-model.h5')
 modelRetino = load_model('retino_detection.h5')
-#modelPnemonia = load_model('pnemonia.h5')
 modelBreast = load_model('final_CNN.h5')
+modelKeratocone=load_model('model_1.h5')
 
 def names(number):
     if(number == 0):
@@ -38,36 +33,43 @@ def names(number):
 
 def retinas(number):
     if(number == 0):
-        return 'Normal'
+        return "Normal"
     elif(number == 1):
-        return 'Mild'
+        return "Mild"
     elif(number == 2):
-        return 'Moderate'
+        return "Moderate"
     elif(number == 3):
-        return 'Severe'
+        return "Severe"
     elif(number == 4):
-        return 'Proliferative'
+        return "Proliferative"
 
 def pneumonia_names(number):
     if (number == 0):
-        return 'Normal'
+        return "Normal"
     elif (number == 1):
-        return 'Pneumonia'
+        return "Pneumonia"
 
 def breast_names(number):
     if (number == 0):
-        return 'Normal'
+        return "Normal"
     elif (number == 1):
-        return 'Breast Cancer'
+        return "Breast Cancer"
+
+def keratocone_names(number):
+    if(number == 0):
+        return "keratocone"
+    elif(number == 1):
+        return "normal"
+    elif(number == 2):
+        return "suspicious"
+
 
 def riddleRetina(n):
     oracle = ""
     # load image and verify if it's an image
     try:
         binary_data = io.BytesIO(n)
-
         img = Image.open(binary_data)
-
     except:
         oracle = "not an image"
 
@@ -150,11 +152,39 @@ def riddleBreast(n):
 
         # Make predictions using the new model
         answ = modelBreast.predict_on_batch(x)
+        print(answ)
         classification = np.where(answ == np.amax(answ))[1][0]
 
         oracle = f"{answ[0][classification] * 100:.2f}% Confidence This Is {breast_names(classification)}"
     except:
         oracle = "bad image"
+
+    return oracle
+
+def riddleKeratocone(file_path):
+    oracle = ""
+    # load image and verify if it's an image
+    try:
+        binary_data = io.BytesIO(file_path)
+        img = Image.open(binary_data)
+    except Exception as e:
+        oracle = f"Error processing image: {str(e)}"
+        return oracle
+    try:
+        # Resize the image to match the expected input shape of the model
+        resized_img = img.resize((128, 128))
+        x = np.array(resized_img)
+        x = x.reshape(1, 128, 128, 3)  # Ensure the image has 3 channels (RGB)
+        x = x / 255.0
+
+        # Make predictions using the new model
+        answ = modelKeratocone.predict_on_batch(x)
+        classification = np.argmax(answ)
+
+
+        oracle = f"{answ[0][classification] * 100:.2f}% Confidence This Is {keratocone_names(classification)}"
+    except Exception as e:
+        oracle = f"Error processing image: {str(e)}"
 
     return oracle
 
@@ -169,12 +199,11 @@ channel.queue_declare(queue='rpc_brain')
 channel.queue_declare(queue='rpc_retino')
 channel.queue_declare(queue='rpc_pneumonia')
 channel.queue_declare(queue='rpc_breast')
+channel.queue_declare(queue='rpc_keratocone')
 
 
 def brain(ch, method, props, body):
-
     #  n = int.from_bytes(body, "big")
-
     print("classifying image...")
     response = riddle(body)
     print(response)
@@ -226,18 +255,32 @@ def breast(ch, method, props, body):
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+def keratocone(ch, method, props, body):
+
+    print("classifying image...")
+    response = riddleKeratocone(body)
+    print(response)
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(
+                         correlation_id=props.correlation_id),
+                     body=str(response))
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
 def main():
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(queue='rpc_brain', on_message_callback=brain)
     channel.basic_consume(queue='rpc_retino', on_message_callback=retino)
     channel.basic_consume(queue='rpc_pneumonia', on_message_callback=pneumonia)
     channel.basic_consume(queue='rpc_breast', on_message_callback=breast)
+    channel.basic_consume(queue='rpc_keratocone', on_message_callback=keratocone)
     print(" [x] Awaiting RPC requests")
     channel.start_consuming()
     print(' [*] Waiting for messages. To exit press CTRL+C')
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
 
     try:
         main()
